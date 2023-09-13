ec654a4b85e2e43905236025ce9131285ad16fd1
emmylem <benjaminosondu55@gmail.com>
2023-09-13 10:48:00 +0000

    uuid update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """auth class"""
 import bcrypt
+from uuid import uuid4
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -11,6 +12,12 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def _generate_uuid() -> str:
+    """Generate a new UUID and return it as a string representation."""
+    new_uuid = uuid.uuid4()
+    return str(new_uuid)
 
 
 class Auth:
@@ -39,3 +46,14 @@ class Auth:
         else:
             return bcrypt.checkpw(password=password.encode(),
                                   hashed_password=user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """Create a session for a user and return the session ID."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+
+            return session_id
+        except as e:
+            return None
